7d2fcacc717dfffb4a62eba85a6ed9db48df4418
Przemek03 <przemyslaw.osuch@uth.pl>
2024-11-13 18:16:20 +0100

    Add files via upload

---
+++ java/Vehicle.java
@@ -0,0 +1,10 @@
+package pl.example.vehicles;
+
+public abstract class Vehicle {
+
+    private String name;
+
+    public Vehicle(String name) {this.name = name;}
+    public abstract int getMaxSpeed();
+    public String getName() {return name;}
+}
